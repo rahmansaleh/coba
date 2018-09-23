892c2c8ed0d875da7ecdd5d01507356eab48ca9b
Muhammad Rahman Saleh <36500308+rahmansaleh@users.noreply.github.com>
2018-09-23 21:24:32 +0700

    Update MainActivity.kt
    dont do it in this case

--- app/src/main/java/com/sti/sijariemas/coba/MainActivity.kt
+++ app/src/main/java/com/sti/sijariemas/coba/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.i("hello", "here")
+        Log.i("hello", "dont do it")
     }
 }
